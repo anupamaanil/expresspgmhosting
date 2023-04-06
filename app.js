5f5512f53966955db067bcab77e214939609dcca
anupamaanil <anupamaanil88@gmail.com>
2023-04-06 22:39:53 +0530

    Message

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 var express=require('express')
 var bodyParser = require('body-parser')
+var mongoose= require('mongoose')
+const { studentModel } = require('./models/studentmodel')
+
+
 
 var app=express()
 app.use(bodyParser.urlencoded({ extended: false }))
   
 app.use(bodyParser.json())
+mongoose.connect("mongodb+srv://anupama:anupama@cluster0.muxgnix.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.set('strictQuery', true);
 
-app.get('/',(req,res)=>{
-    res.send("Welcome Girl :)")
+
+app.get('/ViewAll',async (req,res)=>{
+    try {
+        var result= await studentModel.find(
+            res.json(result)
+        )
+    } 
+    catch (error)
+     {
+        res.send(error)
+    }
+    app.listen(3000,()=>{
+        console.log("Server started")
+    })
 })
 
 app.post('/read',(req,res)=>{
-    var getName=req.body.name
-    var getRoll=req.body.rollno
+   
+    var studentObject=new studentModel(req.body)
+     
+    studentObject.save(
+        (error)=>{
+            if (error)
+            {
+                res.send(error)
+            }
+            else{
+                res.json({"status":"success"})
+            }
 
-    res.json( {"Name":getName,"Roll no":getRoll} )
-
+        }
+    )
 })
 
-
-app.listen( process.env.PORT || 3000,()=>{
+app.listen( 3000,()=>{
     console.log("Server started at http://localhost:3000/")
 })
